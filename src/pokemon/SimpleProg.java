97bae04b93de73cc0f999727ace957db741da32c
A.P. Liang <apandaliang@gmail.com>
2016-09-01 09:47:07 +0800

    Initial
    Initial.

---
+++ src/pokemon/SimpleProg.java
@@ -0,0 +1,19 @@
+package pokemon;
+
+public class SimpleProg {
+	
+		public class Pokémon{
+	    public String type;
+	    public int level;
+	    public String nickname;
+	public Pokémon (String name) {
+}
+	
+	public static void main(String[] args) {
+		Pokémon starter = new Pokémon( "pikachu" );
+
+		    }
+		}
+		
+		
+}
